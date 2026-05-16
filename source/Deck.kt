ba31b7ee017efc65a08544d23a0a891726e707ae
Baccus Li <libaccus@gmail.com>
2026-05-16 13:39:27 +0200

    Use idiomatic Kotlin deck initialization

--- source/Deck.kt
+++ source/Deck.kt
@@ -1,20 +1,15 @@
 package source
 
-import java.util.Collections
-
 class Deck {
 
-    private val cards = ArrayList<Int>()
-
-    init {
-        cards.add(0)
+    private val cards = buildList {
+        add(0)
         for (number in 1..12) {
             repeat(number) {
-                cards.add(number)
+                add(number)
             }
         }
-        Collections.shuffle(cards)
-    }
+    }.shuffled().toMutableList()
 
     fun drawCard(): Int {
         return cards.removeAt(cards.lastIndex)
